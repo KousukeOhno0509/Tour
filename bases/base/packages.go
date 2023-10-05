7f769de530bfbecc7a2a2ce9cced1e8f1f15c8c9
n_sato <n.satou@neuro-tech.jp>
2023-10-05 17:27:45 +0900

    コメント

--- bases/base/packages.go
+++ bases/base/packages.go
@@ -5,8 +5,11 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"time"
 )
 
 func main() {
+	//0から10までのランダムな数値を表示する
+	rand.Seed(time.Now().UnixNano())
 	fmt.Println("My favorite number is", rand.Intn(10))
 }
